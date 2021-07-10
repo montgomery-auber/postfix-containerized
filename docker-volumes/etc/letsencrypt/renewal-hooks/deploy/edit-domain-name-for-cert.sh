b0d9556b5727326a6b750abd011c7386d34eaac7
Montgomery Auber <support@floatingcloud.io>
2021-07-10 22:43:18 +0300

    add ssl to dovecot, maybe it will work

--- docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
+++ docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -x
 sed -i 's/dummy.float.i.ng/'"$FQDN"'/g' /etc/nginx/conf.d/default.conf
+sed -i 's/dummy.float.i.ng/'"$FQDN"'/g' /etc/dovecot/dovecot.conf
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/nginx/restart
+curl -X POST --unix-socket /run/docker.sock http://docker/containers/dovecot/restart
