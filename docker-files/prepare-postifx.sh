0aeb92e1ad192e96fda6a98e313475d008b0e52d
Montgomery Auber <support@floatingcloud.io>
2021-07-14 20:57:51 +0300

    fix prepare script

--- docker-files/prepare-postifx.sh
+++ docker-files/prepare-postifx.sh
@@ -17,53 +17,54 @@ if [ "$UID" != "0" ]; then
 fi
 # put install docker and compose here
 #add user postfix 
-yum remove postfix -y
+sudo yum remove postfix -y
 cd ../docker-volumes
-mkdir -p ./etc/postfix ./var/spool/postfix ./etc/dovecot ./var/spool/mail ./var/log ./var/mail ./var/mail/domains
+sudo mkdir -p ./etc/postfix ./var/spool/postfix ./etc/dovecot ./var/log ./var/mail ./var/mail/domains 
 #chown -R 106:106 ./var/spool/
-chown -R 106:106 ./var/mail/domains #vmail:postdrop
+sudo chown -R 106:106 ./var/mail/domains 
+
+#vmail:postdrop
 #chown root:root ./etc/postfix/dynamicmaps.cf
 #chmod 644 ./etc/postfix/dynamicmaps.cf
 #The following needs to be owned by root
+sudo chown -R root:root ./var/spool/postfix ./etc/postfix/ 
 cd etc/postfix
-#mkdir sql
-#PGPW="byg2812"
-cat - <<EOF >sql/pgsql_virtual_alias_domain_catchall_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_alias_domain_catchall_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
 dbname = postfixadmin
 query = Select goto From alias,alias_domain where alias_domain.alias_domain = '%d' and alias.address = '@' ||  alias_domain.target_domain and alias.active = true and alias_domain.active= true 
 EOF
-cat - <<EOF >sql/pgsql_virtual_alias_domain_mailbox_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_alias_domain_mailbox_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
 dbname = postfixadmin
 query = Select maildir from mailbox,alias_domain where alias_domain.alias_domain = '%d' and mailbox.username = '%u' || '@' || alias_domain.target_domain and mailbox.active = true and alias_domain.active
 EOF
-cat - <<EOF >sql/pgsql_virtual_alias_domain_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_alias_domain_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
 dbname = postfixadmin
 query = select goto from alias,alias_domain where alias_domain.alias_domain='%d' and alias.address = '%u' || '@' || alias_domain.target_domain and alias.active= true and alias_domain.active= true
 EOF
-cat - <<EOF >sql/pgsql_virtual_alias_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_alias_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
 dbname = postfixadmin
 query = Select goto From alias Where address='%s' and active ='1'
 EOF
-cat - <<EOF >sql/pgsql_virtual_domains_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_domains_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
 dbname = postfixadmin
 query = Select domain from domain where domain='%s' and active='1'
 EOF
-cat - <<EOF >sql/pgsql_virtual_mailbox_maps.cf
+sudo cat - <<EOF >sql/pgsql_virtual_mailbox_maps.cf
 user=postfixadmin
 password = $PGPW
 hosts = pgsql
@@ -71,18 +72,19 @@ dbname = postfixadmin
 query = Select maildir from mailbox where username='%s' and active=true
 EOF
 #chown -R postfix:postfix sql
-chown -R 105:105 sql
+sudo chown -R 105:105 sql
 chmod 644 sql/*
 ##Add Dovecot 
 cd ..
 #Create the /etc/dovecot/dovecot-sql.conf file:
-cat - <<EOF > dovecot/dovecot-sql.conf
+sudo cat - <<EOF > dovecot/dovecot-sql.conf
 driver = pgsql
 connect = host=pgsql dbname=postfixadmin user=postfixadmin password=$PGPW
 password_query = select username,password from mailbox where local_part = '%n' and domain = '%d'
 default_pass_scheme =  SHA512-CRYPT
 EOF
-chown root:root dovecot/dovecot-sql.conf
-chmod 600 dovecot/dovecot-sql.conf
+sudo chown root:root dovecot/dovecot-sql.conf
+sudo chmod 600 dovecot/dovecot-sql.conf
 #mkdir -p ./dev/log
 #mkdir -p /opt/postfix/etc/postfix /opt/postfix/var/spool/postfix /opt/postfix/var/spool/mail /opt/postfix/var/log /opt/postfix/var/mail /opt/postfix/var/mail/domains
+ 
