94cd2b27a69904f8404b80fbc3eb91eaa87cd332
Montgomery Auber <support@floatingcloud.io>
2021-07-18 22:35:02 +0300

    add ssl stuff

--- docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
+++ docker-volumes/etc/letsencrypt/renewal-hooks/deploy/edit-domain-name-for-cert.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 sed -i 's/dummy.float.i.ng/'"$FQDN"'/g' /etc/nginx/conf.d/default.conf
 sed -i 's/dummy.float.i.ng/'"$FQDN"'/g' /etc/dovecot/dovecot.conf
+sed -i 's/dummy.float.i.ng/'"$FQDN"'/g' /etc/postfix/main.cf
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/nginx/restart
 curl -X POST --unix-socket /run/docker.sock http://docker/containers/dovecot/restart
+curl -X POST --unix-socket /run/docker.sock http://docker/containers/postfix/restart
