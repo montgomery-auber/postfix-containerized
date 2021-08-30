5fe70b048ea1f6718e880de77e76a4da2ab2887b
Montgomery Auber <support@floatingcloud.io>
2021-08-30 22:07:41 +0300

    change certbot hook so it will always run and clean roundcube imap allow file

--- docker-volumes/var/roundcube/config/imappermissions.php
+++ docker-volumes/var/roundcube/config/imappermissions.php
@@ -1,12 +1,2 @@
-$config['imap_conn_options'] = array(
-'ssl' => array(
-'verify_peer' => false,
-'verfify_peer_name' => false,
-),
-);
-$config['smtp_conn_options'] = array(
-'ssl' => array(
-'verify_peer' => false,
-'verify_peer_name' => false,
-),
-);
+$config['imap_conn_options'] = array( 'ssl' => array( 'verify_peer' => false, 'verfify_peer_name' => false,),);
+$config['smtp_conn_options'] = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false,),);
