20948342d897ccd2654290d0e82cbf22afa4f635
Montgomery Auber <support@floatingcloud.io>
2021-08-31 21:51:49 +0300

    add logging limit to roundcube and limits to prep script

--- docker-files/prepare-postifx.sh
+++ docker-files/prepare-postifx.sh
@@ -2,6 +2,14 @@
 set -x
 # RUN this as root 
 PGPW=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id)
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+if (( EUID != 0 )); then
+    echo "You must be root to do this." 1>&2
+    exit 1
+fi
 # Change file names, so that after git pull config files wont be overwritten with dummy domain
 sudo cp ../docker-volumes/etc/nginx/conf.d/default.conf.dummy ../docker-volumes/etc/nginx/conf.d/default.conf
 sudo cp ../docker-volumes/etc/dovecot/dovecot.conf.dummy  ../docker-volumes/etc/dovecot/dovecot.conf
