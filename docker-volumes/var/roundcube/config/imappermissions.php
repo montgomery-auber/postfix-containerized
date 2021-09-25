f6c76f45594098babd4538a51f179b52b60b3c92
Montgomery Auber <support@floatingcloud.io>
2021-09-25 21:37:52 +0300

    beginning of the end, before GR

--- docker-volumes/var/roundcube/config/imappermissions.php
+++
@@ -1,2 +0,0 @@
-$config['imap_conn_options'] = array( 'ssl' => array( 'verify_peer' => false, 'verfify_peer_name' => false,),);
-$config['smtp_conn_options'] = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false,),);
